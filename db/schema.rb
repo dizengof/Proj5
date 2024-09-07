bccef8bff0ceade650b711a5f0ea309bcfd0036d
dizengof <98748902+dizengof@users.noreply.github.com>
2024-09-07 16:02:19 +0300

    orders scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_232859) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_02_082653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name", limit: 32, null: false
@@ -26,6 +31,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_232859) do
     t.bigint "product_id", null: false
     t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
     t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "producers", force: :cascade do |t|
@@ -48,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_232859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "producer_id"
+    t.integer "discount"
     t.index ["producer_id"], name: "index_products_on_producer_id"
     t.check_constraint "guarantee = ANY (ARRAY[0, 1])", name: "guarantee_check"
     t.check_constraint "price > 0::numeric", name: "price_check"
