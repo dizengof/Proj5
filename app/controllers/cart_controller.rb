edd7df0262f77f4de5d4ccf0e6b8b340fbb90cbe
dizengof <98748902+dizengof@users.noreply.github.com>
2024-09-14 16:30:08 +0300

    Stable ru translation; no design

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -14,9 +14,22 @@ class CartController < ApplicationController
     else
       @cart.items.create(product: @product, quantity:)
     end
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [turbo_stream.replace('cart',
+         partial: 'cart/cart', locals: { cart: @cart }), turbo_stream.replace(@product)]
+      end
+    end
   end
 
   def remove
     Item.find_by(id: params[:id]).destroy
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace('cart',
+        partial: 'cart/cart',
+        locals: { cart: @cart })
+      end
+    end
   end
 end
